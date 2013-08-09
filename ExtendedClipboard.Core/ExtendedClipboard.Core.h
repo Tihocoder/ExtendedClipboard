9f850313f355fa17959c1ff831e7229fbda54510
Tihocoder <tihocoder@gmail.com>
2013-08-09 14:03:27 +0400

    Base solution structure

---
+++ ExtendedClipboard.Core/ExtendedClipboard.Core.h
@@ -0,0 +1,13 @@
+// ExtendedClipboard.Core.h
+
+#pragma once
+
+using namespace System;
+
+namespace ExtendedClipboardCore {
+
+	public ref class Class1
+	{
+		// TODO: Add your methods for this class here.
+	};
+}
